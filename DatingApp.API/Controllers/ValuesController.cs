ecced5b432d854a96b63ffa0e345ecb40153b667
BenjaminHojgaard <benjaminoest@hotmail.com>
2019-08-06 15:54:16 +0200

    ng sign up form

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace DatingApp.API.Controllers
             this._context = context;
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
@@ -30,6 +31,7 @@ namespace DatingApp.API.Controllers
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
